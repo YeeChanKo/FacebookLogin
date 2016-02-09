c4ecb45bd61e616567d428fb75ebf2ca4e324332
YeeChanKo <yeechan.ko@nhnnext.org>
2016-02-10 04:39:30 +0900

    Facebook LogIn Test #1
    simple set-ups
    tried to customize default login button but failed

---
+++ app/src/main/java/com/example/viz/facebooklogin/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.viz.facebooklogin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+
+        setContentView(R.layout.activity_main);
+    }
+}
